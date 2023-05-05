75be46abe2a28b956be75463304f7de635a63717
Neno Arisma <nenoarisma@gmail.com>
2023-05-05 19:31:25 +0700

    chore: update ProductCategory model

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductCategory extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public $fillable = [
+      'name'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Products::class, 'categories_id', 'id')
+    }
 
 }
