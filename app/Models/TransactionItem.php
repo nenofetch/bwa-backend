00dced2a6e51fff9d2706a0a553c9fc3d93deb7b
Neno Arisma <nenoarisma@gmail.com>
2023-05-05 20:36:08 +0700

    chore: update TransactionItem model

--- app/Models/TransactionItem.php
+++ app/Models/TransactionItem.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionItem extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+      'users_id',
+      'products_id',
+      'transactions_id',
+      'quantity',
+    ];
+
+    public function product()
+    {
+        return $this->hasOne(Product::class, 'id', 'products_id');
+    }
 }
