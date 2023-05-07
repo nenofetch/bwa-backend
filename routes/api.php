02fa3f19c0ce8f7d110d83fc0439090a9617c6c6
Neno Arisma <nenoarisma@gmail.com>
2023-05-07 22:11:29 +0700

    chore: update route of categories

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\ProductCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('products', [ProductController::class, 'all']);
-Route::get('categories', [ProductController::class, 'all']);
+Route::get('categories', [ProductCategoryController::class, 'all']);
