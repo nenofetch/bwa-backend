2fd969dcfebb4e716663eb93b0ec9e46c9f22be4
Neno Arisma <nenoarisma@gmail.com>
2023-05-11 05:16:42 +0700

    feat: create factory and seeder of Product

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->name(),
+            'price' => rand(1000, 3000),
+            'description' => $this->faker->realText(),
+            'tags' => $this->faker->slug
+        ];
+    }
+}
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Product::factory(5)->create([
+           'product_categories_id' => rand(1,4)
+        ]);
+    }
+}
