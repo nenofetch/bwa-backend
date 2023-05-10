9265b4dda0556551f42c0534bd9d121963248117
Neno Arisma <nenoarisma@gmail.com>
2023-05-11 04:06:19 +0700

    chore: update name of model Transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Transanction extends Model
+class Transaction extends Model
 {
     use HasFactory, SoftDeletes;
 
