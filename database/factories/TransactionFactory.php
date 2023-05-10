e67615c50b42b95311ef2c0a1a48350de4bf7be6
Neno Arisma <nenoarisma@gmail.com>
2023-05-11 05:21:28 +0700

    feat: create factory and seeder of Transaction

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'address' => $this->faker->address(),
+            'payment' => 'MANUAL',
+            'total_price' => (rand(1000, 2000)),
+            'shipping_price' => (rand(18000, 20000)),
+            'status' => 'Shipped',
+        ];
+    }
+}
