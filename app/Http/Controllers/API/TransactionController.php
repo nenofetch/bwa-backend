6f55e6226d4bc5fc35473b8e21149f704e2b8b71
Neno Arisma <nenoarisma@gmail.com>
2023-05-12 07:49:30 +0700

    feat: create transaction feature

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\TransactionItem;
 use Illuminate\Http\Request;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\Auth;
@@ -40,6 +41,38 @@ class TransactionController extends Controller
         return ResponseFormatter::success(
             $transaction->paginate($limit),
             'Data list transaksi berhasil diambil'
+        );
+    }
+
+    public function checkout(Request $request)
+    {
+        $request->validate([
+            'items' => 'required|array',
+            'items.*.id' => 'exists:products, id',
+            'total_price' => 'required',
+            'shipping_price' => 'required',
+            'status' => 'required|in:SHIPPED,PENDING,SUCCESS,CANCELED,SHIPPING,FAILED'
+        ]);
+
+        $transaction = Transaction::create([
+            'users_id' => Auth::user()->id,
+            'address' => $request->address,
+            'total_price' => $request->total_price,
+            'shipping_price' => $request->shipping_price,
+            'status' => $request->status
+        ]);
+
+        foreach ($request->items as $product) {
+            TransactionItem::create([
+                'users_id' => Auth::user()->id,
+                'products_id' => $product['id'],
+                'transactions_id' => $request->transactions_id,
+                'quantity' => $product['quantity']
+            ]);
+        }
+        return ResponseFormatter::success(
+            $transaction->load('items.product'),
+            'Transaksi berhasil'
         );
     }
 }
