303d3507c7d53a0806994df2767110096d8436d7
Neno Arisma <nenoarisma@gmail.com>
2023-06-10 12:46:38 +0700

    chore: update user controller regsiter

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
                 'name' => ['required', 'string', 'max:255'],
                 'username' => ['required', 'string', 'max:255', 'unique:users'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-                'phone' => ['required', 'string', 'max:13', 'unique:users'],
+                'phone' => ['nullable', 'string', 'max:13', 'unique:users'],
                 'password' => ['required', 'string', new Password]
             ]);
 
